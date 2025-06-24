8bed682d003db4697ad821a59730d87ccaeaca46
Kanyin Erinle <kanyin@meta.com>
2025-06-23 17:45:20 -0700

    Attempting to set up my auth routes.

--- Backend/PrismaClient.js
+++ Backend/PrismaClient.js
@@ -1,2 +1,11 @@
 import { PrismaClient } from "./generated/prisma/index.js";
 const prisma = new PrismaClient();
+export async function createUser(username, password) {
+  return await prisma.user.create({
+    data: {
+      username,
+      password,
+    }
+  })
+}
+export default prisma
