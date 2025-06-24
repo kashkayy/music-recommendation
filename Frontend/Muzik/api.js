3da419d32763525baea7b9ff9e97e70e2152ac86
Kanyin Erinle <kanyin@meta.com>
2025-06-24 01:59:19 -0700

    User can create account from the frontend

--- Frontend/Muzik/api.js
+++
@@ -1,7 +0,0 @@
-const BASE_URL = "http://localhost:5200"
-export async function register(newUser){
-  const {username, password} = newUser
-  const res = await fetch(`${BASE_URL}/auth/signup`, {method:'POST', headers:{'Content-Type': 'application/json'}, body: JSON.stringify(username, password)})
-  const data = await res.json()
-  return data
-}
--- Frontend/Muzik/src/pages/Signup.jsx
+++ Frontend/Muzik/src/pages/Signup.jsx
@@ -1,11 +1,23 @@
 import { useState } from "react"
+import { register } from "../api.js"
 export default function SignUp(){
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  function handleCreateUser(newUser){
+    register(newUser);
+  }
+  function handleSubmit(event){
+    event.preventDefault();
+    const user = {username, password}
+    handleCreateUser(user)
+    setUsername("")
+    setPassword("")
+  }
   return(
     <>
-      <form>
-        <label><input type="text" placeholder="Enter your email" required/></label>
-        <label><input type="text" placeholder="Create a username"  required/></label>
-        <label><input type="text" placeholder="Create a password" required/></label>
+      <form id="sign-up-form" onSubmit={handleSubmit}>
+        <label><input type="text" placeholder="Create a username" value={username} onChange={(event) => setUsername(event.target.value)} required/></label>
+        <label><input type="text" placeholder="Create a password" value={password} onChange={(event) => setPassword(event.target.value)} required/></label>
         <button type="submit" id="sign-up">Sign up!</button>
       </form>
     </>
