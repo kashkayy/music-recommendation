fb2ebd9da25a4057411491e68464e72dc9efcff6
Kanyin Erinle <kanyin@meta.com>
2025-06-23 21:44:39 -0700

    set up route for signup

--- Backend/PrismaClient.js
+++ Backend/PrismaClient.js
@@ -1,11 +1,23 @@
 import { PrismaClient } from "./generated/prisma/index.js";
 const prisma = new PrismaClient();
+import bcrypt from 'bcrypt'
 export async function createUser(username, password) {
-  return await prisma.user.create({
-    data: {
-      username,
-      password,
-    }
-  })
+  const existingUser = await prisma.user.findUnique({
+    where: { username },
+  });
+  if (existingUser) {
+    throw new Error('Username already exists.');
+  }
+  try{
+    const passwordhash = bcrypt.hashSync(password, 15)
+    return await prisma.user.create({
+      data: {
+        username,
+        passwordhash,
+      },
+    })
+  }catch(err){
+    console.log("Error creating user: ", err)
+  }
 }
 export default prisma
