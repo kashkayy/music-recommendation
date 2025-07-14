71a5eea4afaa7b84f5cb73677fe488029a0242fd
Kanyin Erinle <kanyin@meta.com>
2025-07-14 12:33:47 -0700

    refactor: improve zoom bucketing and cluster logic

--- Backend/utils/ZoomHelper.js
+++ Backend/utils/ZoomHelper.js
@@ -1,39 +1,10 @@
-//Maps each google Maps zoom level to an approximate bucket size in degrees
-// higher zoom = smaller buckets and vice versa 
-const bucketSizeByZoomLevel = {
-  1: 5.0,
-  2: 2.5,
-  3: 1.5,
-  4: 1.0,
-  5: 0.5,
-  6: 0.25,
-  7: 0.1,
-  8: 0.05,
-  9: 0.02,
-  10: 0.01,
-  11: 0.005,
-  12: 0.002,
-  13: 0.001,
-  14: 0.0005,
-  15: 0.00025,
-  16: 0.0001,
-  17: 0.00005,
-  18: 0.000025,
-  19: 0.00001,
-  20: 0.000005,
-}
+const worldDiameter_Km = 7000
+const unitConverter = 111.1
 export function getBucketSizeFromZoom(zoom, centerLat = 0) {
-  const base = bucketSizeByZoomLevel[zoom] || 0.0001
-  const bucketLat = base
-  const bucketLng = (base / Math.cos(centerLat * Math.PI / 180))
-  return {bucketLat, bucketLng}
-}
-export function getCoordBounds(latMax, lngMax, latMin, lngMin) {
-  return {
-    lat: { gte: latMin, lte: latMax },
-    lng: { gte: lngMin, lte: lngMax },
-  };
-}
-export function roundedCalculator(coord, bucketCoord) {
-  return Math.floor((coord * bucketCoord) / bucketCoord)
+  const defaultZoom = 2
+  const radius = worldDiameter_Km/(2 ** (zoom - defaultZoom))
+  const bucketLat = radius/unitConverter
+  const safePoint = Math.max(-89.9999, Math.min(89.9999, centerLat))
+  const bucketLng = (bucketLat/ Math.cos(safePoint * Math.PI / 180))
+  return { bucketLat, bucketLng }
 }
