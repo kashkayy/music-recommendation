bb17c873e4e81909fc96fae6f159303a750125a6
Kanyin Erinle <kanyin@meta.com>
2025-07-22 21:33:23 -0700

    feat: implement not found page

--- Muzik/src/components/NotFound.jsx
+++ Muzik/src/components/NotFound.jsx
@@ -3,9 +3,9 @@ export default function NotFound() {
   return (
     <>
       <h1> 404 NOT FOUND </h1>
-      <h2>The page you are looking for does not exist</h2>
+      <h2>The page you are looking for does not exist!</h2>
       <span>
-        Click <Link to="/">here</Link> to return to Sound Map
+        Click <Link to="/">here</Link> to return to Sound Map 🎵
       </span>
     </>
   );
