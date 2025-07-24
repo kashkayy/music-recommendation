05e3a8957e71fe08aa214d84d3e35bb66e53342e
Kanyin Erinle <kanyin@meta.com>
2025-07-23 18:47:33 -0700

    feat: render recommendations in the app

--- Muzik/src/hooks/useRecommendation.jsx
+++ Muzik/src/hooks/useRecommendation.jsx
@@ -3,16 +3,17 @@ import { getUserRecommendation } from "../api";
 export default function useRecommendation() {
   const [recommendations, setRecommendations] = useState([]);
   const [recommendationsLoading, setRecommendationsLoading] = useState(false);
+  const [recommendationsError, setRecommendationsError] = useState(null);
   const getRecommendations = useCallback(async (userLat, userLng, range = 5, userId) => {
     setRecommendationsLoading(true);
     try {
       const response = await getUserRecommendation(userLat, userLng, range, userId);
       setRecommendations(response);
     } catch (error) {
-      throw new Error("Error fetching recommended songs");
+      setRecommendationsError("Error fetching recommended songs, please try again");
     } finally {
       setRecommendationsLoading(false);
     }
   }, []);
-  return { recommendations, getRecommendations, recommendationsLoading }
+  return { recommendations, getRecommendations, recommendationsLoading, recommendationsError }
 }
