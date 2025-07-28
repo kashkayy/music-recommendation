6135e8a00b5fa30aea2228cb3dd64c8dc10ea99e
Kanyin Erinle <kanyin@meta.com>
2025-07-28 13:37:21 -0700

    feat: setup router for svdController

--- Backend/routes/recommendationRoutes.js
+++ Backend/routes/recommendationRoutes.js
@@ -2,6 +2,8 @@ import express from "express";
 import { getUserRecommendations } from "../controllers/recommendationControllers.js";
 import { badReq, successRes } from "../utils/Response.js";
 import { parseObj } from "../utils/ParseObj.js";
+import { getSvdResults } from "../controllers/svdControllers.js";
+import { authenticateToken } from "../middleware/authMiddleware.js";
 const router = express.Router();
 router.get("/", async (req, res) => {
   const parsedObj = parseObj(req.query);
@@ -10,6 +12,18 @@ router.get("/", async (req, res) => {
   }
   try {
     const results = await getUserRecommendations(parsedObj);
+    res.status(200).json(successRes(results));
+  } catch (error) {
+    res.status(500).json(badReq("Error processing request"));
+  }
+});
+router.get("/svd", authenticateToken, async (req, res) => {
+  const userId = req.user.id;
+  if (!userId) {
+    return res.status(400).json(badReq("Invalid id"));
+  }
+  try {
+    const results = await getSvdResults(userId);
     res.status(200).json(successRes(results));
   } catch (error) {
     res.status(500).json(badReq("Error processing request"));
