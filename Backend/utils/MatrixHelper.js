a5885b37beb3a8c888acef70ea0b7d35e304321a
Kanyin Erinle <kanyin@meta.com>
2025-07-27 23:06:35 -0700

    build: compute additional matrix functions.

--- Backend/utils/MatrixHelper.js
+++ Backend/utils/MatrixHelper.js
@@ -54,21 +54,70 @@ function addMultipleMatrices(...matrices) {
   return sumMatrix;
 }
 
-function createVector(n) {
-  let newVector = [];
-  for (let i = 0; i < n; i++) {
-    newVector.push(0);
-  }
-  return newVector;
-}
-
 function dotProduct(v1, v2) {
   if (v1.length !== v2.length) {
-    return null;
+    throw new Error("Unequal lengths between both vectors");
   }
   let result = 0;
   for (let i = 0; i < v1.length; i++) {
     result += v1[i] * v2[i];
   }
   return result;
+}
+
+function matrixVectorMultiply(matrix, vector) {
+  if (matrix.length === 0 || matrix[0].length !== vector.length) {
+    throw new Error(
+      "Matrix columns must match vector length for multiplication."
+    );
+  }
+  const resultVector = [];
+  for (let i = 0; i < matrix.length; i++) {
+    const row = matrix[i];
+    const product = dotProduct(row, vector);
+    resultVector.push(product);
+  }
+  return resultVector;
+}
+
+function transpose(matrix) {
+  const transposed = matrix[0].map((col, i) => matrix.map((row) => row[i]));
+  return transposed;
+}
+// This function returns the result of the multiplication between a matrix and a scalar unit/quantity
+function matrixScalar(matrix, scalar) {
+  if (!Array.isArray(matrix) || !Array.isArray(matrix[0])) {
+    throw new Error("Input must be a matrix");
+  }
+  const result = [];
+  for (let i = 0; i < matrix.length; i++) {
+    result[i] = [];
+    for (let j = 0; j < matrix[i].length; j++) {
+      result[i][j] = matrix[i][j] * scalar;
+    }
+  }
+  return result;
+}
+
+function getVectorMagnitude(vector) {
+  const magnitude = Math.sqrt(
+    vector.reduce((acc, curr) => acc + curr * curr, 0)
+  );
+  return magnitude;
+}
+
+function normalizeVector(vector) {
+  const magnitude = getVectorMagnitude(vector);
+  const normalized = vector.map((element) => element / magnitude);
+  return normalized;
+}
+
+// This function creates a clone of the original matrix and prevents its original properties from being altered.
+function createDeepCopyMatrix(originalMatrix) {
+  return structuredClone(originalMatrix);
+}
+function createRandomVector(n) {
+  return Array(n)
+    .fill(0)
+    .map(() => Math.random());
 }
