0447153bc97ca0c000f35485b3d56b2ee53b7dcd
Kanyin Erinle <kanyin@meta.com>
2025-07-29 00:35:54 -0700

    feat!: setup controller for hybrid recommendations

--- Backend/controllers/recommendationControllers.js
+++ Backend/controllers/recommendationControllers.js
@@ -1,4 +1,5 @@
 import { haverSineDistance } from "../utils/Haversine.js";
+import { getSvdResults } from "./svdControllers.js";
 import prisma from "../PrismaClient.js";
 export async function getUserRecommendations({
   userLat,
@@ -29,6 +30,10 @@ export async function getUserRecommendations({
     const userSavedSongIds = new Set(
       userSavedSongs.map((saved) => saved.songId)
     );
+    function normalizeDistance(distance, min = 50, max = 1) {
+      const norm = (distance - min) / (max - min);
+      return norm;
+    }
     const recommendedSongs = songs
       .map((song) => {
         const distance = haverSineDistance(
@@ -37,7 +42,8 @@ export async function getUserRecommendations({
           song.lat,
           song.lng
         );
-        return { ...song, distance };
+        const normDist = normalizeDistance(distance);
+        return { ...song, distance, normDist };
       })
       .filter((song) => song.distance <= range)
       .filter((song) => !userSavedSongIds.has(song.song.id))
@@ -55,5 +61,71 @@ export async function getUserRecommendations({
     return filtered;
   } catch (error) {
     throw new Error("Error getting recommended songs");
+  }
+}
+
+export async function getHybridRecommendations({
+  userLat,
+  userLng,
+  range,
+  userId,
+  svdWeight = 0.4,
+  locationWeight = 0.6,
+  limit = 15,
+}) {
+  if (!userLat || !userLng || !range || !userId) {
+    throw new Error("Missing required parameters");
+  }
+  try {
+    const svdRecommendations = await getSvdResults(userId);
+    const locationRecommendations = await getUserRecommendations({
+      userLat,
+      userLng,
+      range,
+      userId,
+    });
+    //Initialize a songMap to track which songs is being recommended from both systems
+    const songMap = new Map();
+    svdRecommendations.forEach((song) => {
+      //record normalized svd score
+      const svdScore = song.predictionNormScore;
+      songMap.set(song.id, {
+        song: song,
+        svdScore: svdScore,
+        locationScore: 0,
+        finalScore: 0,
+      });
+    });
+    locationRecommendations.forEach((item) => {
+      const song = item.song;
+      const locationScore = item.normDist;
+
+      if (songMap.has(song.id)) {
+        // Song exists in SVD results, update data with location score
+        const songData = songMap.get(song.id);
+        songData.locationScore = locationScore;
+      } else {
+        // Song only exists in location results
+        songMap.set(song.id, {
+          song: song,
+          svdScore: 0,
+          locationScore: locationScore,
+          finalScore: 0,
+        });
+      }
+    });
+    // Calculate final scores using pre-defined weights
+    for (const [songId, songData] of songMap.entries()) {
+      songData.finalScore =
+        songData.svdScore * svdWeight + songData.locationScore * locationWeight;
+    }
+    const hybridRecommendations = Array.from(songMap.values())
+      .sort((a, b) => b.finalScore - a.finalScore)
+      .slice(0, limit);
+
+    // Return combined recommendations
+    return hybridRecommendations;
+  } catch (error) {
+    throw new Error("Error getting hybrid recommendations");
   }
 }
